5214b13bb2b5c4ce252da9f62261882691b59939
arjungoel16 <arjun.goel6@gmail.com>
2025-02-17 11:22:36 -0500

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HousePricePredictor from "./components/HousePricePredictor";
+import "./index.css"; // Ensure styles are correctly applied
+
+function App() {
+  return (
+    <div className="container">
+      <HousePricePredictor />
+    </div>
+  );
+}
+
+export default App;
